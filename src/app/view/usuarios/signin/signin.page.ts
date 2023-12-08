f22827285a39b73ced7cb777ba69e1760f02f086
Tallys <imakkamikill@gmail.com>
2023-12-08 11:10:31 -0300

    Aula 08/12/23

---
+++ src/app/view/usuarios/signin/signin.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
+import { AuthService } from 'src/app/model/services/auth.service';
+
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.page.html',
+  styleUrls: ['./signin.page.scss'],
+})
+export class SigninPage implements OnInit {
+  formLogar : FormGroup;
+
+  constructor(private alert : AlertService,
+    private authService: AuthService,
+    private router : Router, private formBuilder : FormBuilder) {
+      this.formLogar = new FormGroup({
+        email: new FormControl(''),
+        senha: new FormControl('')
+      });
+     }
+
+  ngOnInit() {
+    this.formLogar = this.formBuilder.group({
+      email : ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get errorControl(){
+    return this.formLogar.controls;
+  }
+
+  submitForm() : boolean {
+    if(!this.formLogar.valid){
+      this.alert.presentAlert('Erro', 'Erro ao Prencher!');
+      return false;
+    }else{
+      this.logar();
+      return true;
+    }
+  }
+
+  private logar(){
+    this.authService.signIn(this.formLogar.value['email'], this.formLogar.value['senha']).then((res)=>{
+      this.alert.presentAlert('Sucesso', 'Login Realizado!'), this.router.navigate(["/home"])
+    }).catch((error)=>{
+      this.alert.presentAlert('Erro', 'Email ou Senha invalidos'),
+      console.log(error.message);
+    })
+
+  }
+
+  logarComGoogle(){}
+
+  irParaSignUp(){
+    this.router.navigate(["/signup"]);
+  }
+
+
+
+}
